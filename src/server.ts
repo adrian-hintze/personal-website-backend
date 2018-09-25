27f4f54a81adf2cb14f5425bea97a5dc5a4b6cd3
Adrian Hintze <hintze.adrian@gmail.com>
2018-09-25 23:24:53 +0100

    Removed HTTPS redirect. Updated app.yaml.

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,6 @@ import * as express from 'express';
 
 const app: express.Express = express();
 
-function forceHttps(req: express.Request, res: express.Response, next: express.NextFunction) {
-    if (req.secure) {
-        return next();
-    };
-
-    res.redirect('https://' + req.hostname + req.url);
-}
-
-// force HTTPS, check if it can be done in app engine
-app.use(forceHttps);
-
 app.get('/', (req, res) => res.send('Under construction...'))
 
 const port = process.env.PORT || 8080;
