f11e5f38b1e50b8d6ba1d78ebf2c9ab2955bb79e
Adrian Hintze <hintze.adrian@gmail.com>
2021-01-28 23:19:49 +0000

    Updated server.js file to work with website.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Under construction...'))
+
+const app = express();
+app.use('/', express.static(path.join(__dirname, 'web')));
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const port = process.env.PORT || 8080;
