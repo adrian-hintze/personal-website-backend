cfcb52a90cdcecef36a52765ca93867915cb5b0b
Adrian Hintze <hintze.adrian@gmail.com>
2021-01-28 23:51:51 +0000

    Updated static folder name.

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 
 const app = express();
-app.use('/', express.static(path.join(__dirname, 'web')));
+app.use('/', express.static(path.join(__dirname, 'personal-website')));
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const port = process.env.PORT || 8080;
