03513b96f483dac41f3db63c3a973bcd12e5dac9
Adrian Hintze <hintze.adrian@gmail.com>
2021-01-28 22:37:46 +0000

    Updated dependencies. Moved from TS to plain JS.

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-import * as express from 'express';
-
-const app: express.Express = express();
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => res.send('Under construction...'))
 
+// Listen to the App Engine-specified port, or 8080 otherwise
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`App listening on port ${port}.`));
